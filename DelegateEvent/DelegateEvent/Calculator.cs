d56cc40793220f9952ade187731b717fe7320746
Clint-Mathews <mathewsclint28@gmail.com>
2022-03-16 09:11:36 +0530

    Added delegate and event project

---
+++ DelegateEvent/DelegateEvent/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEvent
+{
+    public class Calculator
+    {
+        // public delegate int Calculate(int input);
+        // public delegate void Print(int input);
+
+        public event Action<object, CalculatorEventArgs> Calculate;
+
+        public void RaiseEvent(string name)
+        {
+            Calculate?.Invoke(this, new CalculatorEventArgs() { Name = name});
+        }
+
+        public int Execute(Func<int,int> calculate, Action<int> print, int input)
+        {
+            // return calculate.Invoke(input);
+            
+            var res = calculate.Invoke(input);
+            print(res);
+            return res;
+        }
+
+    }
+}
---
+++ DelegateEvent/DelegateEvent/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace DelegateEvent
+{
+    class Program
+    {
+        // static int Square(int input) => input * input;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App Start");
+
+            var calculator = new Calculator();
+
+            //Both works fine since its either a delegate or signature of delegate
+
+            //Calculator.Calculate calc = Square;
+            //var res = calculator.Execute(calc, 5);
+
+            //var res = calculator.Execute(Square, Console.WriteLine, 5);
+            var res = calculator.Execute((x) => x*x , Console.WriteLine, 5);
+            // calculator.Calculate += Calculator_Calculate;
+            calculator.Calculate += (obj, args) => Console.WriteLine(args.Name);
+            calculator.RaiseEvent("Clint");
+            //Console.WriteLine(res);
+
+            Console.WriteLine("App Stop");
+
+        }
+
+        private static void Calculator_Calculate(object arg1, CalculatorEventArgs arg2)
+        {
+            Console.WriteLine(arg2.Name);
+        }
+    }
+}
