502a78e1669d3209e7e3f5d2528f8e6ae30891de
Clint-Mathews <mathewsclint28@gmail.com>
2022-03-17 08:44:47 +0530

    Added sample exception handling

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using ExceptionHandling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Test("Test");
+    }
+    static void Test(string fileName)
+    {
+        try
+        {
+            if(String.IsNullOrEmpty(fileName)) throw new CustomException("File name should not be null or empty");
+            if (File.Exists(fileName))
+            {
+                File.OpenRead(fileName);
+            } else
+            {
+                var data = new CustomException();
+                data.Data.Add("Key", "Hello");
+                throw data;
+            }
+        }
+        catch (CustomException e) when (e.Data.Contains("Key"))
+        {
+            Console.WriteLine(e);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        finally
+        {
+            Console.WriteLine("Excecution Done");
+        }
+    }
+}
